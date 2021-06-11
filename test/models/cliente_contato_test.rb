cb98e7ebc68648415258ca6676b5c2e2634e04a6
GVGTEC <gvgtec@terra.com.br>
2021-06-10 22:36:28 -0300

    delete pasta test

--- test/models/cliente_contato_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ClienteContatoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/contas_pagar_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ContasPagarTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
