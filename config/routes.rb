1f216b3b90b48a002a10e01cff37affab9ff1beb
Thiago Malaquias <tammalaquias@gmail.com>
2021-06-16 21:08:49 -0300

    fix layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
   resources :estoques
   resources :vendedores
-  resources :transportadora_contatos
   resources :transportadoras
-  resources :contas_pagar_parcelas
   resources :contas_pag
   resources :plano_contas
   resources :empresas
   resources :produtos
   resources :localizacao_estoques
-  resources :cliente_contatos
   resources :clientes
-  resources :fornecedor_contatos
   resources :fornecedores
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
