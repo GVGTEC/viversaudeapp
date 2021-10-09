fdfbe5d642f711ee3dab22bda9dbb4bcf718c63b
Thiago Malaquias <tammalaquias@gmail.com>
2021-10-08 22:08:38 -0300

    correcoes standard cfop controller

--- app/controllers/cfop_controller.rb
+++ app/controllers/cfop_controller.rb
@@ -1,13 +1,13 @@
 class CfopController < ApplicationController
-  before_action :set_cfop, only: %i[ show edit update destroy ]
+  before_action :set_cfop, only: %i[show edit update destroy]
 
   # GET /cfop or /cfop.json
   def index
     @cfop = Cfop.where(empresa_id: @adm.empresa.id)
 
     # paginação na view index (lista)
-    options = {page: params[:page] || 1, per_page: 50} 
-    @cfop = @cfop.paginate(options)    
+    options = {page: params[:page] || 1, per_page: 50}
+    @cfop = @cfop.paginate(options)
   end
 
   # GET /cfop/1 or /cfop/1.json
@@ -26,21 +26,19 @@ class CfopController < ApplicationController
   # POST /cfop or /cfop.json
   def create
     respond_to do |format|
-      begin
-        params[:codigo].each do |key, value|
-          @cfop = Cfop.new(cfop_params)
-          @cfop.codigo = value
-          @cfop.informativo = key
-          @cfop.empresa_id = @adm.empresa.id
-          @cfop.save
-        end
-
-        format.html { redirect_to cfop_index_path, notice: "Cfop Cadastrado" }
-        format.json { render :show, status: :created, location: @cfop }
-      rescue => exception
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @cfop.errors, status: :unprocessable_entity }
+      params[:codigo].each do |key, value|
+        @cfop = Cfop.new(cfop_params)
+        @cfop.codigo = value
+        @cfop.informativo = key
+        @cfop.empresa_id = @adm.empresa.id
+        @cfop.save
       end
+
+      format.html { redirect_to cfop_index_path, notice: "Cfop Cadastrado" }
+      format.json { render :show, status: :created, location: @cfop }
+    rescue
+      format.html { render :new, status: :unprocessable_entity }
+      format.json { render json: @cfop.errors, status: :unprocessable_entity }
     end
   end
 
@@ -69,20 +67,21 @@ class CfopController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_cfop
-      @cfop = Cfop.find(params[:id])
-    end
 
-    def atualiza_cfops_com_mesma_natureza
-      cfops = Cfop.where(natureza_operacao: @natureza)
-      cfops.each do |cfop|
-        cfop.update(cfop_params)
-      end
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_cfop
+    @cfop = Cfop.find(params[:id])
+  end
 
-    # Only allow a licfopsst of trusted parameters through.
-    def cfop_params
-      params.require(:cfop).permit(:descricao, :natureza_operacao, :natureza_operacao_st, :operacao, :nota_complementar_impostos_sn, :entrada_saida_es, :cliente_fornecedor_cf, :calcular_impostos_sn, :faturamento_sn, :observacao)
+  def atualiza_cfops_com_mesma_natureza
+    cfops = Cfop.where(natureza_operacao: @natureza)
+    cfops.each do |cfop|
+      cfop.update(cfop_params)
     end
+  end
+
+  # Only allow a licfopsst of trusted parameters through.
+  def cfop_params
+    params.require(:cfop).permit(:descricao, :natureza_operacao, :natureza_operacao_st, :operacao, :nota_complementar_impostos_sn, :entrada_saida_es, :cliente_fornecedor_cf, :calcular_impostos_sn, :faturamento_sn, :observacao)
+  end
 end
