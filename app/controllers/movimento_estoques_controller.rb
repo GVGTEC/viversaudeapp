a66c0c23acc48ec425aa602e03c0a510030b89ca
Thiago Malaquias <tammalaquias@gmail.com>
2021-10-08 22:17:15 -0300

    correcoes standard movimento controller

--- app/controllers/movimento_estoques_controller.rb
+++ app/controllers/movimento_estoques_controller.rb
@@ -6,12 +6,13 @@ class MovimentoEstoquesController < ApplicationController
     @movimento_estoques = MovimentoEstoque.where(produto_id: @produto.id)
 
     # paginação na view index (lista)
-    options = {page: params[:page] || 1, per_page: 50} 
-    @movimento_estoques = @movimento_estoques.paginate(options)    
+    options = {page: params[:page] || 1, per_page: 50}
+    @movimento_estoques = @movimento_estoques.paginate(options)
   end
 
   private
-    def set_produto
-      @produto = Produto.find(params[:produto_id])
-    end
+
+  def set_produto
+    @produto = Produto.find(params[:produto_id])
+  end
 end
