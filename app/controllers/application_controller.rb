0dbe29657b52d364dbe1d7838a5c983cfd764071
Thiago Malaquias <tammalaquias@gmail.com>
2021-10-08 22:08:25 -0300

    correcoes standard application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action  :authenticate_user!
+  before_action :authenticate_user!
 
   def authenticate_user!
     if cookies[:admin_viver_saude].blank?
       redirect_to "/login"
     else
-      ApplicationRecord.administrador_record=(administrador)
+      ApplicationRecord.administrador_record = (administrador)
       administrador
     end
   end
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
     if cookies[:admin_viver_saude].present?
       return @adm if @adm.present?
       @adm = Administrador.find(JSON.parse(cookies[:admin_viver_saude])["id"])
-      return @adm
+      @adm
     end
   end
 end
