ace6258eca7374b8be0612069e1cb4d63c06aeb6
Thiago Malaquias <tammalaquias@gmail.com>
2021-10-08 22:21:07 -0300

    correcoes standard orçamento controller

--- app/controllers/orcamentos_controller.rb
+++ app/controllers/orcamentos_controller.rb
@@ -1,14 +1,14 @@
 class OrcamentosController < ApplicationController
-  before_action :set_orcamento, only: %i[ show edit update destroy ]
+  before_action :set_orcamento, only: %i[show edit update destroy]
 
   # GET /orcamentos or /orcamentos.json
   def index
     @orcamentos = Orcamento.all
-    #@orcamentos = Orcamento.where(empresa_id: @adm.empresa.id)   O CAMPO EMPRESA_ID AINDA NÃO EXISTE NA TABELA DE ORÇAMENTO
+    # @orcamentos = Orcamento.where(empresa_id: @adm.empresa.id)   O CAMPO EMPRESA_ID AINDA NÃO EXISTE NA TABELA DE ORÇAMENTO
 
     # paginação na view index (lista)
-    options = {page: params[:page] || 1, per_page: 50} 
-    @orcamentos = @orcamentos.paginate(options)       
+    options = {page: params[:page] || 1, per_page: 50}
+    @orcamentos = @orcamentos.paginate(options)
   end
 
   # GET /orcamentos/1 or /orcamentos/1.json
@@ -27,11 +27,11 @@ class OrcamentosController < ApplicationController
   # POST /orcamentos or /orcamentos.json
   def create
     @orcamento = Orcamento.new(orcamento_params)
-    #@orcamento.empresa_id = @adm.empresa.id
+    # @orcamento.empresa_id = @adm.empresa.id
 
     respond_to do |format|
       if @orcamento.save
-        #format.html { redirect_to @orcamento, notice: "Orçamento Cadastrado" }
+        # format.html { redirect_to @orcamento, notice: "Orçamento Cadastrado" }
         format.html { redirect_to new_orcamento_orcamento_item_path(@orcamento), notice: "Orçamento Cadastrado" }
         format.json { render :show, status: :created, location: @orcamento }
       else
@@ -64,13 +64,14 @@ class OrcamentosController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_orcamento
-      @orcamento = Orcamento.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def orcamento_params
-      params.require(:orcamento).permit(:cliente_id, :vendedor_id, :data_emissao, :valor_total, :observacao, :flag)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_orcamento
+    @orcamento = Orcamento.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def orcamento_params
+    params.require(:orcamento).permit(:cliente_id, :vendedor_id, :data_emissao, :valor_total, :observacao, :flag)
+  end
 end
