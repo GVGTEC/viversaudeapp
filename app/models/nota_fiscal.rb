f544c28dbd127f03149607cb2b4f6c86bcbe9c6d
Thiago Malaquias <tammalaquias@gmail.com>
2021-10-08 22:26:26 -0300

    correcoes standard nota fiscal model

--- app/models/nota_fiscal.rb
+++ app/models/nota_fiscal.rb
@@ -1,11 +1,13 @@
 class NotaFiscal < ApplicationRecord
+  validates :transportadora_id, presence: true
+
   belongs_to :cfop, optional: true
   belongs_to :cliente, optional: true
   belongs_to :fornecedor, optional: true
   belongs_to :vendedor, optional: true
   belongs_to :transportadora, optional: true
   belongs_to :empresa
-  
+
   before_validation :salvar_empresa
 
   has_one :nota_fiscal_transporta, dependent: :destroy
