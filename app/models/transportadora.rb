5c321a5efed981a83862831efdb3aae874221e29
Thiago Malaquias <tammalaquias@gmail.com>
2021-10-08 22:26:47 -0300

    correcoes standard transportadora model

--- app/models/transportadora.rb
+++ app/models/transportadora.rb
@@ -2,7 +2,7 @@ class Transportadora < ApplicationRecord
   belongs_to :empresa
 
   def contatos
-    Contato.where(natureza: "transportadoras", natureza_id: self.id)
+    Contato.where(natureza: "transportadoras", natureza_id: id)
   rescue
     []
   end
