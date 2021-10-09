98e60adcf52940b05a409e149240204eccc0eb22
Thiago Malaquias <tammalaquias@gmail.com>
2021-10-08 22:26:16 -0300

    correcoes standard fornecedor model

--- app/models/fornecedor.rb
+++ app/models/fornecedor.rb
@@ -1,9 +1,9 @@
 class Fornecedor < ApplicationRecord
   has_many :produtos, dependent: :destroy
   belongs_to :empresa
-  
+
   def contatos
-    Contato.where(natureza: "fornecedores", natureza_id: self.id)
+    Contato.where(natureza: "fornecedores", natureza_id: id)
   rescue
     []
   end
