76e4a3b4ebc068aec3ab1b2a85bd8494048aee27
Thiago Malaquias <tammalaquias@gmail.com>
2021-10-12 08:22:03 -0300

    iniciando duplicata com 001

--- app/controllers/nota_fiscal_duplicatas_controller.rb
+++ app/controllers/nota_fiscal_duplicatas_controller.rb
@@ -21,18 +21,13 @@ class NotaFiscalDuplicatasController < ApplicationController
 
   private
 
-  # Only allow a list of trusted parameters through.
-  def nota_fiscal_params
-    params.require(:nota_fiscal).permit(:numero_nota, :numero_pedido, :cfop_id, :entsai, :cliente_id, :fornecedor_id, :vendedor_id, :data_emissao, :data_saida, :hora_saida, :valor_desconto, :valor_produtos, :valor_total_nota, :valor_frete, :valor_outras_despesas, :numero_pedido_compra, :tipo_pagamento, :meio_pagamento, :numero_parcelas_pagamento, :observacao, :chave_acesso_nfe, :nota_cancelada_sn)
-  end
-
   def salvar_vencimento_parcelas
     if params[:nota_fiscal].has_key?(:nota_fiscal_faturamento_parcelas)
       NotaFiscalFaturamentoParcela.where(nota_fiscal: @nota_fiscal.id).destroy_all
-      params[:nota_fiscal][:nota_fiscal_faturamento_parcelas].each do |faturamento_parcela|
+      params[:nota_fiscal][:nota_fiscal_faturamento_parcelas].each_with_index do |faturamento_parcela, i|
         @nota_fiscal_faturamento_parcela = NotaFiscalFaturamentoParcela.new
         @nota_fiscal_faturamento_parcela.nota_fiscal = @nota_fiscal
-        @nota_fiscal_faturamento_parcela.duplicata = rand(1..999).to_s
+        @nota_fiscal_faturamento_parcela.duplicata = formatar_numero_duplicata(i + 1)
         @nota_fiscal_faturamento_parcela.prazo_pagamento = faturamento_parcela[:prazo_pagamento]
         @nota_fiscal_faturamento_parcela.data_vencimento = faturamento_parcela[:data_vencimento]
         @nota_fiscal_faturamento_parcela.valor_parcela = faturamento_parcela[:valor_parcela]
@@ -41,10 +36,19 @@ class NotaFiscalDuplicatasController < ApplicationController
     end
 
     respond_to do |format|
-      format.html { redirect_to nota_fiscais_path, notice: "Nota fiscal item Cadastrado" }
+      format.html { redirect_to observacoes_nota_fiscal_path(@nota_fiscal), notice: "Nota fiscal item Cadastrado" }
     end
   end
 
+  def formatar_numero_duplicata(numero)
+    sprintf '%03d', numero
+  end
+
+  # Only allow a list of trusted parameters through.
+  def nota_fiscal_params
+    params.require(:nota_fiscal).permit(:numero_nota, :numero_pedido, :cfop_id, :entsai, :cliente_id, :fornecedor_id, :vendedor_id, :data_emissao, :data_saida, :hora_saida, :valor_desconto, :valor_produtos, :valor_total_nota, :valor_frete, :valor_outras_despesas, :numero_pedido_compra, :tipo_pagamento, :meio_pagamento, :numero_parcelas_pagamento, :observacao, :chave_acesso_nfe, :nota_cancelada_sn)
+  end
+  
   def set_nota_fiscal
     @nota_fiscal = NotaFiscal.find(params[:nota_fiscal_id])
   end
